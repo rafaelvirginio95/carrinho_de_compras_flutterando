e3f49fbccc7c44e0be770dd9b15aa69fe57d8a3f
Rafael Virginio <32203155+rafaelvirginio95@users.noreply.github.com>
2021-10-22 09:39:56 -0300

    Add files via upload

---
+++ lib/app/modules/compra/compra_controller.g.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'compra_controller.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$CompraController on _CompraControllerBase, Store {
+  final _$_CompraControllerBaseActionController =
+      ActionController(name: '_CompraControllerBase');
+
+  @override
+  void adicionarNoCarrinho(ProdutoModel produto) {
+    final _$actionInfo = _$_CompraControllerBaseActionController.startAction(
+        name: '_CompraControllerBase.adicionarNoCarrinho');
+    try {
+      return super.adicionarNoCarrinho(produto);
+    } finally {
+      _$_CompraControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+
+    ''';
+  }
+}
